fa1de9a4a9fcf93404665a9388bae1773a9d505f
Yussif Yahuza <yussifyahuza12@gmail.com>
2024-09-17 20:11:40 +0000

    Refactor layout component to make it responsive and add page transition animation

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import SideNav from "../shared/sidenav";
 import TopNav, { UserData } from "../shared/dashboard-topnav";
 
@@ -27,8 +28,20 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     setIsCollapsed(!isCollapsed);
   };
 
+  const pageVariants = {
+    initial: { opacity: 0, x: -10 },
+    in: { opacity: 1, x: 0 },
+    out: { opacity: 0, x: 10 },
+  };
+
+  const pageTransition = {
+    type: "tween",
+    ease: "anticipate",
+    duration: 0.5,
+  };
+
   return (
-    <div className="flex w-full h-screen overflow-hidden">
+    <div className="flex w-full h-screen overflow-hidden bg-gray-50">
       <SideNav
         isCollapsed={isCollapsed}
         toggleCollapse={toggleSidebar}
@@ -37,9 +50,16 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       />
       <div className="flex w-full flex-col flex-grow">
         <TopNav user={user} toggleSidebar={toggleSidebar} />
-        <main className="flex-grow p-6 bg-gray-100 overflow-auto w-full">
+        <motion.main
+          className="flex-grow p-6 overflow-auto w-full"
+          initial="initial"
+          animate="in"
+          exit="out"
+          variants={pageVariants}
+          transition={pageTransition}
+        >
           {children}
-        </main>
+        </motion.main>
       </div>
     </div>
   );
